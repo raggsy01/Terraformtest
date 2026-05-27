04e61478e0b72434508bbaa02cf5c8be79499ab6
Tim Reynolds <tim.rey1@googlemail.com>
2026-05-27 15:43:40 +0000

    initial commit

---
+++ main.tf
@@ -0,0 +1,28 @@
+provider "aws" {
+  region = "eu-west-2"
+}
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd-gp3/ubuntu-noble-24.04-amd64-server-*"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+resource "aws_instance" "app_server" {
+  ami           = data.aws_ami.ubuntu.id
+  count         = 2
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "learn-terraform"
+  }
+}
+
+resource "aws_s3_bucket" "s3" {
+  bucket = "tr-terraform-test159"
+}
